c4308daf1769a93deaef3a3d250b6da6fc464705
MaxFalchenco <falchenkomaxim@gmail.com>
2016-03-23 09:21:51 +0200

    final

--- src/main/java/org/sourceit/HomeTask2.java
+++ src/main/java/org/sourceit/HomeTask2.java
@@ -1,6 +1,8 @@
 package org.sourceit;
 
-public class HomeTask2 {
+import java.util.Random;
+
+public class HomeTask2  {
 
     /**
      * Конвертирует десятичное число в бинарную форму
@@ -9,7 +11,21 @@ public class HomeTask2 {
      * @return бинарная форма числа
      */
     public static long decimalToBinary(int number) {
-        return -1;
+
+        int help_rez = 0;
+        long result = 0;
+        int mult = 1;
+
+        while (number != 0)
+        {
+            help_rez = number % 2;
+            result = result + help_rez * mult;
+            number = number / 2;
+            mult *= 10;
+        }
+
+        return result;
+
     }
 
     /**
@@ -19,7 +35,23 @@ public class HomeTask2 {
      * @return октальная форма числа
      */
     public static long decimalToOctal(int number) {
-        return -1;
+
+        int help_rez = 0;
+        int result = 0;
+        int mult = 1;
+
+        while (number != 0)
+        {
+            help_rez = number % 8;
+
+            result = result + help_rez * mult;
+            mult *= 10;
+            number /= 8;
+
+        }
+
+        return result;
+
     }
 
     /**
@@ -28,9 +60,60 @@ public class HomeTask2 {
      * @param number может быть только позитивным
      * @return хексовая форма числа
      */
-    public static long decimalToHex(int number) {
-        return -1;
+    public static long decimalToHex(int number)  {
+        int help_rez = 0;
+        int fin_result = 0;
+        String result = "";
+        char result1 = 0 ;
+        String p1 = "";
+
+
+        while (number != 0)       //   записывать по 1 цифре
+        {
+            help_rez = number % 16;
+
+            if (help_rez == 10)
+                result = result + "01";
+            else
+            if (help_rez == 11)
+                result = result + "11";
+            else
+            if (help_rez == 12)
+                result = result + "21";
+            else
+            if (help_rez == 13)
+                result = result + "31";
+            else
+            if (help_rez == 14)
+                result = result + "41";
+            else
+            if (help_rez == 15)
+                result = result + "51";
+            else
+            {
+                result = result + help_rez;
+            }
+
+            number /= 16;
+
+
+        }
+
+
+        String result2 = "";
+
+        for(int i = result.length()-1; i >= 0; i--)
+        {
+            result2 =  result2 + result.charAt(i);
+        }
+
+        fin_result = Integer.valueOf(result2);
+
+        return fin_result;
+
+
     }
+
 
     /**
      * Конвертирует бинарную форму в десятичное число
@@ -39,8 +122,73 @@ public class HomeTask2 {
      * @return десятичное число
      */
     public static int binaryToDecimal(long binary) {
-        return -1;
+        // System.out.println(binary);
+
+        int i;
+        int count = 0;
+        long sec_bin = binary;
+        double result = 0;
+
+        while( binary > 0) {
+            binary /= 10;
+            count++;
+        }
+
+
+        long [] numbers = new long[count];
+
+        for (i = count - 1; i >= 0; i--)
+        {
+            numbers [i] = sec_bin % 10;
+            sec_bin /= 10;
+        }
+
+
+
+
+
+      /*  System.out.println("11111111111111111111111111111");
+        for (i = 0; i<= count -1; i++)
+        {
+            System.out.println( numbers [i]);
+        }
+        System.out.println("11111111111111111111111111111");*/
+
+
+
+        long [] count_1 = new long[count];
+        int cc = count-1;
+
+        for (i =0; i <= count - 1; i++)
+        {
+            count_1 [i] = cc;
+            cc--;
+        }
+
+
+       /* System.out.println("11111111111111111111111111111");
+        for (i = 0; i<= count -1; i++)
+        {
+            System.out.println( count_1 [i]);
+        }
+        System.out.println("11111111111111111111111111111");*/
+
+        for (i = 0; i<= count -1; i++)
+        {
+
+            //System.out.println(count);
+            result = result + (numbers[i] * Math.pow(2,count_1[i]));
+
+            //System.out.println(result);
+
+        }
+
+        int fin_res= ( int ) result;;
+
+        return fin_res;
+
     }
+
 
     /**
      * Конвертирует октальную форму в десятичное число
@@ -48,9 +196,50 @@ public class HomeTask2 {
      * @param octal октальная форма, может быть только позитивным
      * @return десятичное число
      */
-    public static int octalToDecimal(long octal) {
-        return -1;
+    public static int octalToDecimal(long octal){
+        // System.out.println(binary);
+
+        int i;
+        int count = 0;
+        long sec_bin = octal;
+        double result = 0;
+
+        while( octal > 0) {
+            octal /= 10;
+            count++;
+        }
+
+
+        long [] numbers = new long[count];
+
+        for (i = count - 1; i >= 0; i--)
+        {
+            numbers [i] = sec_bin % 10;
+            sec_bin /= 10;
+        }
+
+        long [] count_1 = new long[count];
+        int cc = count-1;
+
+        for (i =0; i <= count - 1; i++)
+        {
+            count_1 [i] = cc;
+            cc--;
+        }
+
+        for (i = 0; i<= count -1; i++)
+        {
+
+            result = result + (numbers[i] * Math.pow(8,count_1[i]));
+
+        }
+
+        int fin_res= ( int ) result;
+
+        return fin_res;
+
     }
+
 
     /**
      * Конвертирует хексовую форму в десятичное число
@@ -58,8 +247,68 @@ public class HomeTask2 {
      * @param hex хексовая форма, может быть только позитивным
      * @return десятичное число
      */
-    public static int hexToDecimal(long hex) {
-        return -1;
+    public static int hexToDecimal(long hex){
+        // System.out.println(binary);
+
+        int i;
+        int count = 0;
+        long sec_bin = hex;
+        double result = 0;
+
+        while( hex > 0) {
+            hex /= 10;
+            count++;
+        }
+
+
+        long [] count_1 = new long[count];
+        int cc = count-1;
+
+        for (i =0; i <= count - 1; i++)
+        {
+            count_1 [i] = cc;
+            cc--;
+
+        }
+
+
+
+        long [] numbers = new long[count];
+
+        for (i = count - 1; i >= 0; i--)
+        {
+            numbers [i] = sec_bin % 100;
+            if (numbers[i] == 10 || numbers[i] == 11 || numbers[i] == 12 || numbers[i] == 13 || numbers[i] == 14 || numbers[i] == 15)
+            {
+
+                numbers [i] = sec_bin % 100;
+                sec_bin /= 100;
+
+            }
+            else
+            if (numbers[i] != 10 || numbers[i] != 11 || numbers[i] != 12 || numbers[i] != 13 || numbers[i] != 14 || numbers[i] != 15)
+            {
+                numbers [i] = sec_bin % 10;
+                sec_bin /= 10;
+
+            }
+
+        }
+
+
+        for (i = 0; i<= count -1; i++)
+        {
+
+            result = result + (numbers[i] * Math.pow(16,count_1[i]));
+
+        }
+
+
+
+        int fin_res= ( int ) result;;
+
+
+        return fin_res;
     }
 
     /**
@@ -69,8 +318,23 @@ public class HomeTask2 {
      * @param columns
      * @return двумерный массив
      */
-    public static int[][] generateTwoDimensionArray(int rows, int columns) {
-        return null;
+    public static int[][] generateTwoDimensionArray(int rows, int columns){
+        int i, j;
+        int [][] numbers = new int[rows][columns];
+        Random rnd = new Random(System.currentTimeMillis());
+
+
+        for (i = 0; i<= rows-1; i++)
+            for (j = 0; j<= rows-1; j++)
+            {
+                numbers[i][j] = 0 + rnd.nextInt(1000 - 0 + 1);
+            }
+
+        /*for (i = 0; i<= rows-1; i++)
+            for (j = 0; j<= rows-1; j++)
+                System.out.println(numbers[i][j] );*/
+
+        return numbers;
     }
 
     /**
@@ -82,9 +346,37 @@ public class HomeTask2 {
      * @param input двумерный массив
      * @return индекс строки
      */
-    public static int findMaxProduct(int[][] input) {
-        return 1;
+    public static int findMaxProduct(int[][] input){
+        int i,j;
+        int max = 0;
+        int help;
+        int index = 0;
+
+        for(i = 0; i < input.length; i++)
+        {
+            help = 1;
+
+            for (j = 0; j < input.length; j++)
+            {
+
+                help = help * input[i][j];
+
+            }
+
+            if ( help > max )
+            {
+                index = i;
+                max = help;
+
+            }
+
+
+        }
+
+
+        return index;
     }
+
 
     /**
      * Выводит все простые числа из интервала от 2 до n.
@@ -93,7 +385,47 @@ public class HomeTask2 {
      * @return массив простых чисел.
      */
     public static int[] getSimple(int n) {
-        return null;
+
+        int i,j;
+        int count = 0;
+        boolean for_simple = false;
+        int count_for_max = 0;
+
+        for (j = 2; j <= n; j++)
+        {
+            for_simple = false;
+
+            for ( i = 2; i < j - 1; i++)
+            {
+                if (j % i == 0)
+                    for_simple = true;
+            }
+
+            if (for_simple != true)
+                count++;
+        }
+
+        int [] numbers = new int[count];
+
+        for (j = 2; j <= n; j++)
+        {
+            for_simple = false;
+
+            for ( i = 2; i < j - 1; i++)
+            {
+                if (j % i == 0)
+                    for_simple = true;
+            }
+
+            if (for_simple != true)
+            {
+                numbers[count_for_max] = j;
+                count_for_max++;
+            }
+
+        }
+
+        return numbers;
     }
 
     // Рекурсивные методы. Реализовать их нужно с помощью рекурсии.
@@ -104,8 +436,14 @@ public class HomeTask2 {
      * @param n предел
      * @return сумма
      */
-    public static long sum(int n) {
-        return 0;
+    public static long sum(int n)
+    {
+
+        if ( n > 0)
+       return (n>1) ? n+sum(n-1): n;
+        else
+            return (n<1) ? n+sum(n+1): n;
+
     }
 
     /**
@@ -115,8 +453,22 @@ public class HomeTask2 {
      * @param second
      * @return
      */
-    public static int product(int first, int second) {
-        return -1;
+    public static int product(int first, int second)
+    {
+        if( second > 1)
+        {
+            return first + product(first, --second);
+        }
+
+        else
+        if (second < -1 && first < -1)
+        {
+            return first + product(Math.abs(first), Math.abs(--second));
+
+        }
+
+        return first;
+
     }
 
 }
